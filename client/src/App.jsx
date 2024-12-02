1572bdb330eaad829e09ff72fba94f61310058c7
Vinay Patel <vinaypatel5519@gmail.com>
2024-12-02 16:48:20 +0530

    feat: Implement complete authentication flow with Sign Up & Sign In functionality.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const App = () => {
+export default function App() {
   return (
-    <div className="text-cyan-300 bg-red-400">App</div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
-
-export default App
